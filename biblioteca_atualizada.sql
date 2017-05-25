10b026a39a3a11dd4f06c799f56eac6535d0aaff
viniciush4 <viniciush4@gmail.com>
2017-05-24 22:45:39 -0300

    Adicionadas chaves estrangeiras

--- biblioteca_atualizada.sql
+++ biblioteca_atualizada.sql
@@ -1,284 +1,464 @@
-
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 24, 2017 at 07:47 PM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 5.2.17
+-- Host: 127.0.0.1
+-- Generation Time: 25-Maio-2017 às 03:40
+-- Versão do servidor: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u255978174_bibli`
+-- Database: `biblioteca`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `autor`
+-- Estrutura da tabela `autor`
 --
 
-CREATE TABLE IF NOT EXISTS `autor` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `autor` (
+  `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `celular` int(13) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `celular` int(13) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `autorlivro`
+-- Estrutura da tabela `autorlivro`
 --
 
-CREATE TABLE IF NOT EXISTS `autorlivro` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `autorlivro` (
+  `id` int(11) NOT NULL,
   `autor_id` int(11) NOT NULL,
-  `livro_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `autor_id` (`autor_id`),
-  KEY `livro_id` (`livro_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `livro_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `biblioteca`
+-- Estrutura da tabela `biblioteca`
 --
 
-CREATE TABLE IF NOT EXISTS `biblioteca` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `biblioteca` (
+  `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `emprestimo_dias` int(11) NOT NULL DEFAULT '15',
-  `multa_diaria` decimal(2,2) NOT NULL DEFAULT '0.00',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `multa_diaria` decimal(2,2) NOT NULL DEFAULT '0.00'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departamento`
+-- Estrutura da tabela `departamento`
 --
 
-CREATE TABLE IF NOT EXISTS `departamento` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `departamento` (
+  `id` int(11) NOT NULL,
   `biblioteca_id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `biblioteca_id` (`biblioteca_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `nome` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `editora`
+-- Estrutura da tabela `editora`
 --
 
-CREATE TABLE IF NOT EXISTS `editora` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `editora` (
+  `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `emprestimo`
+-- Estrutura da tabela `emprestimo`
 --
 
-CREATE TABLE IF NOT EXISTS `emprestimo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `emprestimo` (
+  `id` int(11) NOT NULL,
   `exemplar_id` int(11) NOT NULL,
   `funcionario_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `data_emprestimo` datetime NOT NULL,
   `data_devolucao` datetime NOT NULL,
-  `valor_multa` decimal(3,2) NOT NULL DEFAULT '0.00',
-  PRIMARY KEY (`id`),
-  KEY `exemplar_id` (`exemplar_id`),
-  KEY `funcionario_id` (`funcionario_id`),
-  KEY `usuario_id` (`usuario_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `valor_multa` decimal(3,2) NOT NULL DEFAULT '0.00'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `exemplar`
+-- Estrutura da tabela `exemplar`
 --
 
-CREATE TABLE IF NOT EXISTS `exemplar` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `exemplar` (
+  `id` int(11) NOT NULL,
   `livro_id` int(11) NOT NULL,
   `secao_biblioteca_id` int(11) NOT NULL,
   `data_cadastro` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `emprestado` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `livro_id` (`livro_id`),
-  KEY `secao_biblioteca_id` (`secao_biblioteca_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `emprestado` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `funcionario`
+-- Estrutura da tabela `funcionario`
 --
 
-CREATE TABLE IF NOT EXISTS `funcionario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `funcionario` (
+  `id` int(11) NOT NULL,
   `departamento_id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `celular` int(11) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
-  `data_contratacao` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `departamento_id` (`departamento_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `data_contratacao` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `livro`
+-- Estrutura da tabela `livro`
 --
 
-CREATE TABLE IF NOT EXISTS `livro` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
   `editora_id` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `ano` year(4) NOT NULL,
-  `isbn` int(13) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `isbn` int(13) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `multa`
+-- Estrutura da tabela `multa`
 --
 
-CREATE TABLE IF NOT EXISTS `multa` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `multa` (
+  `id` int(11) NOT NULL,
   `funcionario_id` int(11) NOT NULL,
   `emprestimo_id` int(11) NOT NULL,
   `data_recebimento` datetime NOT NULL,
-  `valor` decimal(3,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `multa_ibfk_1` (`funcionario_id`),
-  KEY `multa_ibfk_2` (`emprestimo_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `valor` decimal(3,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `palavrachave`
+-- Estrutura da tabela `palavrachave`
 --
 
-CREATE TABLE IF NOT EXISTS `palavrachave` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `palavra` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `palavrachave` (
+  `id` int(11) NOT NULL,
+  `palavra` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `palavrachavelivro`
+-- Estrutura da tabela `palavrachavelivro`
 --
 
-CREATE TABLE IF NOT EXISTS `palavrachavelivro` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `palavrachavelivro` (
+  `id` int(11) NOT NULL,
   `palavra_chave_id` int(11) NOT NULL,
-  `livro_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `livro_id` (`livro_id`),
-  KEY `palavra_chave_id` (`palavra_chave_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `livro_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reserva`
+-- Estrutura da tabela `reserva`
 --
 
-CREATE TABLE IF NOT EXISTS `reserva` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
   `exemplar_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `data` datetime NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `exemplar_id` (`exemplar_id`),
-  KEY `usuario_id` (`usuario_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `status` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `secao`
+-- Estrutura da tabela `secao`
 --
 
-CREATE TABLE IF NOT EXISTS `secao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `secao` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `secaobiblioteca`
+-- Estrutura da tabela `secaobiblioteca`
 --
 
-CREATE TABLE IF NOT EXISTS `secaobiblioteca` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `secaobiblioteca` (
+  `id` int(11) NOT NULL,
   `secao_id` int(11) NOT NULL,
-  `biblioteca_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `secao_id` (`secao_id`),
-  KEY `biblioteca_id` (`biblioteca_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `biblioteca_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `celular` int(11) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
-  `data_nascimento` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `data_nascimento` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuariobiblioteca`
+-- Estrutura da tabela `usuariobiblioteca`
 --
 
-CREATE TABLE IF NOT EXISTS `usuariobiblioteca` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuariobiblioteca` (
+  `id` int(11) NOT NULL,
   `biblioteca_id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `biblioteca_id` (`biblioteca_id`),
-  KEY `usuario_id` (`usuario_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `usuario_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `autorlivro`
+--
+ALTER TABLE `autorlivro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor_id` (`autor_id`),
+  ADD KEY `livro_id` (`livro_id`);
+
+--
+-- Indexes for table `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `biblioteca_id` (`biblioteca_id`);
+
+--
+-- Indexes for table `editora`
+--
+ALTER TABLE `editora`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `exemplar_id` (`exemplar_id`),
+  ADD KEY `funcionario_id` (`funcionario_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indexes for table `exemplar`
+--
+ALTER TABLE `exemplar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `livro_id` (`livro_id`),
+  ADD KEY `secao_biblioteca_id` (`secao_biblioteca_id`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departamento_id` (`departamento_id`);
+
+--
+-- Indexes for table `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `multa`
+--
+ALTER TABLE `multa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `multa_ibfk_1` (`funcionario_id`),
+  ADD KEY `multa_ibfk_2` (`emprestimo_id`);
+
+--
+-- Indexes for table `palavrachave`
+--
+ALTER TABLE `palavrachave`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `palavrachavelivro`
+--
+ALTER TABLE `palavrachavelivro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `livro_id` (`livro_id`),
+  ADD KEY `palavra_chave_id` (`palavra_chave_id`);
+
+--
+-- Indexes for table `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `exemplar_id` (`exemplar_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indexes for table `secao`
+--
+ALTER TABLE `secao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `secaobiblioteca`
+--
+ALTER TABLE `secaobiblioteca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `secao_id` (`secao_id`),
+  ADD KEY `biblioteca_id` (`biblioteca_id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuariobiblioteca`
+--
+ALTER TABLE `usuariobiblioteca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `biblioteca_id` (`biblioteca_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `autorlivro`
+--
+ALTER TABLE `autorlivro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `editora`
+--
+ALTER TABLE `editora`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `exemplar`
+--
+ALTER TABLE `exemplar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `multa`
+--
+ALTER TABLE `multa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `palavrachave`
+--
+ALTER TABLE `palavrachave`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `palavrachavelivro`
+--
+ALTER TABLE `palavrachavelivro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `secao`
+--
+ALTER TABLE `secao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `secaobiblioteca`
+--
+ALTER TABLE `secaobiblioteca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuariobiblioteca`
+--
+ALTER TABLE `usuariobiblioteca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
